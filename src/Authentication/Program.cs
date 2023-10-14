9c22bf9ab3cb46b5483e23973012a98563a3c154
RobinB87 <robinbette@hotmail.com>
2023-10-14 18:34:01 +0000

    setup required code for auth

--- src/Authentication/Program.cs
+++ src/Authentication/Program.cs
@@ -1,13 +1,30 @@
+using System.Security.Claims;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // this is not a jwt token
 builder.Services.AddAuthentication().AddBearerToken(IdentityConstants.BearerScheme);
 builder.Services.AddAuthorizationBuilder();
+builder.Services.AddDbContext<AppDbContext>(x => x.UseSqlite("DataSource=app.db"));
+builder.Services.AddIdentityCore<MyUser>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddApiEndpoints();
 
 var app = builder.Build();
+
+app.MapIdentityApi<MyUser>();
+
+app.MapGet("/", (ClaimsPrincipal user) => $"Hello {user.Identity!.Name}")
+    .RequireAuthorization();
 
 app.Run();
 
 class MyUser : IdentityUser {}
+
+class AppDbContext : IdentityDbContext<MyUser>
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+}

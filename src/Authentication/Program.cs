411b4d949ce2b6c5d44229c5d540e31690b9311d
RobinB87 <robinbette@hotmail.com>
2023-10-14 14:49:07 +0000

    add bearer token

--- src/Authentication/Program.cs
+++ src/Authentication/Program.cs
@@ -1,5 +1,13 @@
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// this is not a jwt token
+builder.Services.AddAuthentication().AddBearerToken(IdentityConstants.BearerScheme);
+builder.Services.AddAuthorizationBuilder();
 
 var app = builder.Build();
 
 app.Run();
+
+class MyUser : IdentityUser {}
